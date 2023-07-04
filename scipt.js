d07f93bc7466e5087990cf3e73e27b59df7fcf63
ADITYA0920 <adityamane21880@gmail.com>
2023-07-04 16:11:41 +0530

    second

--- scipt.js
+++ scipt.js
@@ -1,17 +1,16 @@
 sessionStorage.setItem('IP',"");
+
+
+$.getJSON("https://api.ipify.org?format=json", function(data) {
+         
+sessionStorage.setItem('IP',data.ip) ;
+document.getElementById("ip").innerHTML = data.ip ;
+
+})
 function getData(){
     console.log("click");
-    $.getJSON("https://api.ipify.org?format=json", function(data) {
-         
-        // Setting text of element P with id gfg
-        console.log(typeof(data.ip));
-        sessionStorage.setItem('IP',data.ip) ;
-        document.getElementById("ip").innerHTML = data.ip ;
+    
+        
         window.location.href = '/home' ;  
-    })
-
-    // setTimeout(() => {
-    //    window.location.href = '/home' ;  
-    // }, 2000);
    
 }
